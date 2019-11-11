656bdb6cd3d94eb807a9e560a075336327e12a84
Brooke Bachman <bbachman1994@gmail.com>
2019-11-11 20:22:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -30,9 +30,13 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
+  return aoh
 end
 
 def aoh_update(aoh, row, key, new_value)
+  aoh[row][key] = new_value
+  return aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
